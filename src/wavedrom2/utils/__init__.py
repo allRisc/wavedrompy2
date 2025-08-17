dcd0192d7476620ad805b0b562da6feba6abe197
allRisc <benjamin.j.davis96@gmail.com>
2025-08-17 16:09:29 -0700

    Moved text_width calculation to utils module

---
+++ src/wavedrom2/utils/__init__.py
@@ -0,0 +1,12 @@
+# MIT License
+#
+# Copyright (c) 2025 Benjamin Davis
+#
+# This software is licensed under the MIT License.
+# See the LICENSE file in the project root for the full license text.
+
+from .render import text_width
+
+__all__ = [
+    "text_width",
+]
